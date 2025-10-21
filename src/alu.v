fe13af2975111cf645a5021c1bfc9174e4c26a4d
Sarthak Omar <155576105+code-samurai777@users.noreply.github.com>
2025-10-22 02:38:53 +0530

    Update alu.v

--- src/alu.v
+++ src/alu.v
@@ -1,1 +1,21 @@
-
+module alu(
+    input [7:0] A,
+    input [7:0] B,
+    input [3:0] opcode,
+    output [7:0] Result,
+    output CarryOut,
+    output Zero,
+    output Overflow,
+    output Negative
+);
+    alu_operations alu_inst(
+        .A(A),
+        .B(B),
+        .opcode(opcode),
+        .Result(Result),
+        .CarryOut(CarryOut),
+        .Zero(Zero),
+        .Overflow(Overflow),
+        .Negative(Negative)
+    );
+endmodule

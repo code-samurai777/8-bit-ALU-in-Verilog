b9b64cf85703a1d6a38cc7a75b6d5c8a74c6d337
Sarthak Omar <155576105+code-samurai777@users.noreply.github.com>
2025-10-22 02:38:27 +0530

    Update alu_control.v

--- src/alu_control.v
+++ src/alu_control.v
@@ -1,19 +1,6 @@
-module alu_control (
-    input [2:0] opcode,
-    output reg [7:0] result,
-    output reg carry_out, zero, overflow, negative
+module alu_control(
+    input [3:0] instr,
+    output [3:0] alu_op
 );
-    wire [7:0] A = 8'b00000001; // Example operand A
-    wire [7:0] B = 8'b00000001; // Example operand B
-
-    alu_operations alu_inst (
-        .A(A),
-        .B(B),
-        .opcode(opcode),
-        .result(result),
-        .carry_out(carry_out),
-        .zero(zero),
-        .overflow(overflow),
-        .negative(negative)
-    );
+    assign alu_op = instr; 
 endmodule
